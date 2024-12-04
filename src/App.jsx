17da6b7401cc64f90d3b99e2699f6f51d2b81023
Ron Magpantay <ronmagpantay9513@gmail.com>
2024-12-03 16:29:17 -0800

    Remove unnecessary imports and additional boilerplate clearing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import './scss/main.scss';
-import Header from './components/Header'
-
-// Mantine Imports
-
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <MantineProvider><div>Cleared App</div></MantineProvider>
