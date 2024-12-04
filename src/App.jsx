5c4b4d66eff77f38d7fcdd55bea5e6781842c33e
Ron Magpantay <ronmagpantay9513@gmail.com>
2024-12-03 16:04:02 -0800

    Add scss folder, Vite ENV practice, others

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import './scss/main.scss';
 import Header from './components/Header'
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Test Change</div>
+    <div> { import.meta.env.VITE_API_URL } </div>
   )
 }
 
