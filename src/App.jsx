dc2720e8c3c768cc6952d37e21517f43a9c7c509
Ron Magpantay <ronmagpantay9513@gmail.com>
2024-12-03 21:46:33 -0800

    Add button with variant property

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, Button } from '@mantine/core';
 
 function App() {
 
   return (
     <MantineProvider forceColorScheme='dark'>
         <h1>Cleared App</h1>
+        <Button variant='filled'>Click Here</Button>
     </MantineProvider>
   )
 }
