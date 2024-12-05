64f52a60fde4eaf8815c4078211cc88e29f75a73
Ron Magpantay <ronmagpantay9513@gmail.com>
2024-12-04 21:56:48 -0800

    Add Header component with successful display

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { MantineProvider, Button } from '@mantine/core';
 // Styles import to add ALL MantineUI styles
 import '@mantine/core/styles.css';
 
+// Component Imports
+import { Header } from './components/'
+
 function App() {
 
   return (
     <MantineProvider forceColorScheme='dark'>
         <h1>Cleared App</h1>
+        <Header />
         <Button>Click Here</Button>
     </MantineProvider>
   )
